4687666580b2bbe86f6825855610c67a2e9ef6e6
rhobar121 <rhobar121@gmail.com>
2023-11-24 08:58:33 +0100

    ;

--- src/main/java/agents/CarBuyer.java
+++ src/main/java/agents/CarBuyer.java
@@ -3,4 +3,8 @@ package agents;
 import jade.core.Agent;
 
 public class CarBuyer extends Agent{
+    @Override
+    protected void setup(){
+        System.out.println("SHIT2");
+    }
 }
--- src/main/java/agents/CarSeller.java
+++ src/main/java/agents/CarSeller.java
@@ -3,4 +3,9 @@ package agents;
 import jade.core.Agent;
 
 public class CarSeller extends Agent {
+    @Override
+    protected void setup(){
+        System.out.println("SHIT1");
+    }
+
 }
